fd7f6a9d578660eedce2126f8f85a2357c3da969
ly.ho <ly.ho@asiantech.vn>
2017-04-18 19:56:06 +0700

    - config firebase

--- app/src/main/java/com/dut/moneytracker/models/firebase/FireBaseSync.java
+++ app/src/main/java/com/dut/moneytracker/models/firebase/FireBaseSync.java
@@ -19,6 +19,10 @@ public class FireBaseSync {
     private static final String TAG = FireBaseSync.class.getSimpleName();
     private DatabaseReference mDatabase;
     private static FireBaseSync fireBaseSync;
+    private static final String CHILD_ACCOUNT = "account";
+    private static final String CHILD_EXCHANGE = "exchange";
+    private static final String CHILD_EXCHANGE_LOOP = "exchange_loop";
+    private static final String CHILD_DEBIT = "debit";
 
     public static FireBaseSync getInstance(Context context) {
         if (fireBaseSync == null) {
@@ -32,9 +36,8 @@ public class FireBaseSync {
         mDatabase = FirebaseDatabase.getInstance().getReference(reference);
     }
 
-    public void loadDataCategory() {
-        Log.d(TAG, "loadDataCategory: aaaaa");
-        mDatabase.child("aaa").setValue("abfas");
+    public void upDateAccount() {
+  /*   mDatabase.child("account").child("2").setValue("aaaaaaaaaa");
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -45,8 +48,37 @@ public class FireBaseSync {
             public void onCancelled(DatabaseError databaseError) {
                 Log.d(TAG, "onCancelled: " + databaseError.getMessage());
             }
-        });
+        });*/
+    }
 
+    public void upDateExchange() {
+
+    }
+
+    public void upDateExchangeLoop() {
+
+    }
+
+    public void upDateDebit() {
+
+    }
+
+    public void onLoadDataServer() {
+        Log.d(TAG, "onLoadDataServer: aaaaa");
+
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot messageSnapshot: dataSnapshot.child(CHILD_DEBIT).getChildren()) {
+                    Log.d(TAG, "onDataChange: "+messageSnapshot.getValue());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
