fa98d9d446e4462ab3180cd89f4675980351d92e
ly.ho <ly.ho@asiantech.vn>
2017-04-27 17:28:59 +0700

    - change method name

--- app/src/main/java/com/dut/moneytracker/utils/DateTimeUtils.java
+++ app/src/main/java/com/dut/moneytracker/utils/DateTimeUtils.java
@@ -115,7 +115,7 @@ public class DateTimeUtils {
         return dates;
     }
 
-    public Date getNextDate(Date currentDate, int step) {
+    public Date getSkipDate(Date currentDate, int step) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(currentDate);
         calendar.add(Calendar.DAY_OF_MONTH, step);
