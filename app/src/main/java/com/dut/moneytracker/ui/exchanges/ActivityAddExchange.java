6eaa9c4742c444d179c15fbf235d4048f7497d18
ly.ho <ly.ho@asiantech.vn>
2017-05-17 14:40:55 +0700

    - update ui

--- app/src/main/java/com/dut/moneytracker/ui/exchanges/ActivityAddExchange.java
+++ app/src/main/java/com/dut/moneytracker/ui/exchanges/ActivityAddExchange.java
@@ -235,7 +235,7 @@ public class ActivityAddExchange extends AppCompatActivity implements AddListene
         isClickTabExpense = false;
         mExchange.setTypeExchange(ExchangeType.INCOME);
         mExchange.setIdCategory(null);
-        tvTitleFromAccount.setText(getString(R.string.main_account));
+        tvTitleFromAccount.setText(getString(R.string.name_wallet));
         tvTitleToAccount.setText(getString(R.string.category_name));
         tvCategoryName.setText(getString(R.string.unknown));
         tvStatus.setVisibility(View.VISIBLE);
@@ -254,7 +254,7 @@ public class ActivityAddExchange extends AppCompatActivity implements AddListene
         isClickTabIncome = false;
         mExchange.setTypeExchange(ExchangeType.EXPENSES);
         mExchange.setIdCategory(null);
-        tvTitleFromAccount.setText(getString(R.string.main_account));
+        tvTitleFromAccount.setText(getString(R.string.name_wallet));
         tvTitleToAccount.setText(getString(R.string.category_name));
         tvCategoryName.setText(getString(R.string.unknown));
         tvStatus.setVisibility(View.VISIBLE);
