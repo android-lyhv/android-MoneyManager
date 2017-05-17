92ccac738882b6516b44b6b095517225b3be08a8
ly.ho <ly.ho@asiantech.vn>
2017-05-17 12:54:45 +0700

    - fix inni value calculator

--- app/src/main/java/com/dut/moneytracker/adapter/loop/LoopExchangeAdapter.java
+++ app/src/main/java/com/dut/moneytracker/adapter/loop/LoopExchangeAdapter.java
@@ -109,13 +109,6 @@ public class LoopExchangeAdapter extends BaseRecyclerAdapter {
             } else {
                 tvAmount.setTextColor(ContextCompat.getColor(getContext(), android.R.color.holo_red_light));
             }
-            if (exchangeLooper.isLoop()) {
-                tvStatus.setText(R.string.loop_status_ok);
-                tvStatus.setTextColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
-            } else {
-                tvStatus.setText(R.string.loop_status_close);
-                tvStatus.setTextColor(ContextCompat.getColor(getContext(), android.R.color.holo_red_light));
-            }
         }
 
         @Override
